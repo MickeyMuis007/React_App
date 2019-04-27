ba34c618c712881af5a8c1e1e1efcf43f644845e
= <=>
2019-04-27 22:39:39 +0200

    Using the useSet() Hook for State Manipulation

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    person: [
+const App = props => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
       { name: 'Max', age:'28' },
       { name: 'Manu', age:'29' },
       { name: 'Stephanie', age:'26' }
     ]
-  }
+  });
 
-  switchNameHandler = () => {
-    console.log('Was clickd');
-  }
+  const [otherState, setOtherState] = useState('Some other value');
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, from React App!</h1>
-        <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-        <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
-        <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
-      </div>
-    );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m a React App!!!'));
+  console.log(personsState, otherState, setOtherState);
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        { name: 'Maximilian', age:'28' },
+        { name: 'Manu', age:'29' },
+        { name: 'Stephanie', age:'27' }
+      ]
+    });
   }
+  
+  return (
+    <div className="App">
+      <h1>Hello, from React App!</h1>
+      <p>This is really working</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+    </div>
+  );
 }
 
 export default App;
